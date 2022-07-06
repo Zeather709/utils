470a35ab04dd96f09a560e324b81429b77b9fa7c
Zeather709 <heatherzurel@gmail.com>
2022-07-06 11:42:02 -0230

    util function to time execution of python code

---
+++ python/time-code-execution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 10:41:14 2022
+
+@author: zeather
+"""
+
+# add this before & after code to time execution
+
+import time
+
+start_time = time.time()
+
+# insert code here
+
+print('running time: ',(time.time()-start_time)/60,'min')
